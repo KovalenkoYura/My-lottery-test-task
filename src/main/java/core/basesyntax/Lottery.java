8cb075c726fae52cfbd072cd5b4979804ead2c88
YuraKovalenko <urakovalenko001@gmail.com>
2022-12-14 16:03:33 +0200

    minor fix

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -1,12 +1,10 @@
 package core.basesyntax;
 
 public class Lottery {
+  private final ColorSupplier colorSupplier = new ColorSupplier();
+  private final NumberSupplier numberSupplier = new NumberSupplier();
+
   public Ball getRandomBall() {
-    ColorSupplier colorSupplier = new ColorSupplier();
-    NumberSupplier numberSupplier = new NumberSupplier();
-    Ball ball = new Ball();
-    ball.setColor(colorSupplier.getRandomColor());
-    ball.setNumber(numberSupplier.getRandomNumber());
-    return ball;
+    return new Ball(colorSupplier.getRandomColor(), numberSupplier.getRandomNumber());
   }
 }
